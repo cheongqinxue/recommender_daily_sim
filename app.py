1c8ed110b3a7b7f4c44daacd52149c20f31cc5fa
cheongqinxue <58881356+cheongqinxue@users.noreply.github.com>
2022-01-18 14:21:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -223,19 +223,13 @@ def main(args):
 
 if __name__ == '__main__':
     args = Args()
-    
-    with st.sidebar.form("password", clear_on_submit=False):
-        
-        pwd_ = st.text_input(label='Enter Password')
+           
+    pwd_ = st.sidebar.text_input(label='Enter Password')
 
-        # Every form must have a submit button.
-        submitted = st.form_submit_button("Submit")
-    
-    if submitted:
-        if pwd_ == st.secrets['PASSWORD']:
-            st.balloons()
-            main(args)
-        else:
-            st.error('Wrong password')
-            st.stop()
+    if pwd_ == st.secrets['PASSWORD']:
+        st.balloons()
+        main(args)
+    else:
+        st.error('Wrong password')
+        st.stop()
 
